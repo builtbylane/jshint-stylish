9c9b7563103ca2eee8748c42ee09fa53d0c42da7
Lane <lanegoldberg@gmail.com>
2014-02-15 16:24:27 -0500

    betta

--- stylish.js
+++ stylish.js
@@ -14,10 +14,11 @@ module.exports = {
 		ret += table(result.map(function (el, i) {
 			var err = el.error;
 			var line = [
-				chalk.blue('jshint -' + err.code),
                 chalk.gray(err.id.slice(1,-4)), // remove first and last char
-                chalk.gray('col ' + err.character),
-				chalk.blue('line ' + err.line),
+                chalk.blue('jshint -' + err.code),
+                chalk.gray('line'),
+				chalk.blue(err.line),
+                chalk.gray(':' + err.character),
 				chalk.green(err.reason)
 			];
 
